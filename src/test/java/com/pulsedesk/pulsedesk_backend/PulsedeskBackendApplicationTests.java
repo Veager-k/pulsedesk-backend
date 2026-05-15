4d3606c540d7778409c9ed51aa983d748e247e9b
Veager-k <viliussaukstas@gmail.com>
2026-05-15 14:10:23 +0300

    Initial Spring Boot setup

---
+++ src/test/java/com/pulsedesk/pulsedesk_backend/PulsedeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsedesk.pulsedesk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsedeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
